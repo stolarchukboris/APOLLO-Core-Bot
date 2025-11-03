83e6f3ecd4d252cb5f4c314efe1d7a21ee0f6a04
stolarchukboris <kotkotovic07@gmail.com>
2025-11-04 00:08:46 +0300

    lets throw an error

--- src/commands/general/a_hey.js
+++ src/commands/general/a_hey.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from 'discord.js';
-
+throw new Error();
 export const data = new SlashCommandBuilder()
     .setName('a_hey')
     .setDescription('Make the bot say "Hey!"');
